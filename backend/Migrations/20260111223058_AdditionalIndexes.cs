593acb6cc21b36fd9b5e181e1c41c3421829ada2
Dominic Saladin <dominic@dsaladin.ch>
2026-01-11 23:31:19 +0100

    Add composite index on UserId and Date in investment table

---
+++ backend/Migrations/20260111223058_AdditionalIndexes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DSaladin.Frnq.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_investment_UserId",
+                table: "investment");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_investment_UserId_Date",
+                table: "investment",
+                columns: new[] { "UserId", "Date" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_investment_UserId_Date",
+                table: "investment");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_investment_UserId",
+                table: "investment",
+                column: "UserId");
+        }
+    }
+}
